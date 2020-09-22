b24363d2547fc92a90b2894bcb5108551a5fd935
Srikanth kante <Srikanth.Kante@in.pega.com>
2020-09-22 16:08:52 +0530

    removed rc

--- master.js
+++ master.js
@@ -1,16 +1,3 @@
 master updates
 master alpha
 master beta
-master rc
-master rc1
-edited master
-master
-master final release
-master rc10
-rc11
-rc21
-rc31
-
-
-
-master release 100master release 200
